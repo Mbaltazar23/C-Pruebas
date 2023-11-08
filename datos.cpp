1a71977f16fa7c66baa4421c8e8aa2b4dc7e1962
mbaltazar <74512126+Mbaltazar23@users.noreply.github.com>
2023-11-08 12:14:48 -0300

    realizado hace años y guardado en mi pc

---
+++ datos.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num = 15;
+    float flotante = 10.45;
+    double mayor = 16.575756;
+    char letra = 'a';
+    cout<<mayor + num + flotante;
+
+	return 0;
+}
---
+++ leer_datos.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int numero; //variable numerica
+	
+	cout<<"Digite un numero : ";
+	cin>>numero;
+	
+	cout<<"\nEl numero que digito es: "<<numero;
+	
+	return 0;
+}
